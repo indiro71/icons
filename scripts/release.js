b9194018b59053409d67cd8aed6957e548f1e712
mickle <git@indiro.ru>
2023-01-20 14:20:30 +0300

    Add images

--- scripts/release.js
+++ scripts/release.js
@@ -1,34 +1,16 @@
-const fs = require('fs-extra');
-const path = require('path');
 const execSync = require('child_process').execSync;
-const version = require('../package.json').version;
-
 const exec = command => execSync(command, { stdio: 'inherit' });
 
-const BUILD_PATH = path.join(__dirname, '..', 'lib');
-
-const getPackageJsonSource = ({ version }) => `{
-  "name": "@indiro/react-icons",
-  "version": "${version}",
-  "main": "index.esm.js/index.js",
-  "types": "index.d.ts"
-}`;
-
 const publishLib = async () => {
-  const pj = {
-    filepath: 'package.json',
-    source: getPackageJsonSource({ version })
-  }
-
-  await fs.outputFile(path.join(BUILD_PATH, pj.filepath), pj.source);
-
   console.log('publish')
 
-  exec('git status');
+  exec('git stash');
+  exec('npm version patch');
+  exec('git stash pop');
   exec('git add -A');
   exec('git commit -m "Add images"');
   exec('git push');
-  exec(`cd ${BUILD_PATH} && npm publish --access=public`);
+  exec(`npm publish --access=public`);
 }
 
 publishLib();
