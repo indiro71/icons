24944c3e45bed89d830a06f5d1febcbf7b142866
mickle <git@indiro.ru>
2023-01-24 16:30:16 +0300

    edit structure

--- scripts/release.js
+++ scripts/release.js
@@ -1,17 +1,25 @@
+const fs = require('fs-extra');
+const path = require('path');
 const execSync = require('child_process').execSync;
+
 const exec = command => execSync(command, { stdio: 'inherit' });
+
+const BUILD_PATH = path.join(__dirname, '..', 'lib');
+
 
 const publishLib = async () => {
   console.log('publish')
+
   exec('npm run build');
   exec('git stash');
   exec('npm version patch');
   exec('git stash pop');
+  await fs.copyFile(path.join(__dirname, '..', 'package.json'), path.join(BUILD_PATH, 'package.json'));
   exec('git status');
   exec('git add -A');
   exec('git commit -m "Add images"');
   exec('git push');
-  exec('npm publish --access=public');
+  exec(`cd ${BUILD_PATH} && npm publish --access=public`);
 }
 
 publishLib();
